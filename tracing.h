cd7d64b82c29bfcfcf4dfd48484808c6c294c620
Imran Ashraf <Imran.Ashraf@ymail.com>
2012-11-03 12:57:52 +0100

    Added liscence header

--- tracing.h
+++ tracing.h
@@ -1,10 +1,64 @@
 /*
- * tracing.h
- * 
- * Authour : S. Arash Ostadzadeh (ostadzadeh@gmail.com)
- * Authour : Roel Meeuws (r.j.meeuws@gmail.com)
+
+QUAD
+
+This tool is part of QUAD Toolset
+http://sourceforge.net/projects/quadtoolset
+
+Copyright © 2008-2011 Arash Ostadzadeh (ostadzadeh@gmail.com)
+http://ce.et.tudelft.nl/~arash/
+
+
+This file is part of QUADcore.
+
+QUADcore is free software: you can redistribute it and/or modify 
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of 
+the License, or (at your option) any later version.
+
+QUADcore is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU Lesser General Public License for more details. You should have 
+received a copy of the GNU Lesser General Public License along with QUADcore.
+If not, see <http://www.gnu.org/licenses/>.
+
+
+<LEGAL NOTICE>
+--------------
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.  Redistributions
+in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution. The names of the contributors 
+must be retained to endorse or promote products derived from this software.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ITS CONTRIBUTORS 
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+//==============================================================================
+/* tracing.h: 
+ * The prototypes of tracing routines, used by QUAD tool
  *
- */
+ *  Authors: Arash Ostadzadeh
+ *           Roel Meeuws
+*/
+//==============================================================================
 
 #ifndef __TRACING__H__
 #define __TRACING__H__
